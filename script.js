4921c5636406053344b27db537b897c76e73c6d5
Gerard <gemalvalk@gmail.com>
2023-10-18 20:20:06 -0500

    js functions to scroll down slowlyafter click on link. from chatGTP

--- script.js
+++ script.js
@@ -29,6 +29,10 @@ menu_responsive.classList.toggle("active");
 };
 
 
+const imprimir  = (texto) => {
+    console.log(texto);
+}
+imprimir("hola mundo");
 // window.addEventListener("scroll", function() {
 //     var body = document.body;
 //     if (body.scrollTop > 0) {
@@ -37,3 +41,40 @@ menu_responsive.classList.toggle("active");
 //         body.classList.remove("white-background"); // Remove the new background style
 //     }
 // });
+
+
+// Function to scroll to a target element at a certain speed
+function scrollToTarget(target, speed) {
+    const targetElement = document.querySelector(target);
+    if (targetElement) {
+      const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY;
+      const startPosition = window.scrollY;
+      const distance = targetPosition - startPosition;
+      const startTime = performance.now();
+      const duration = Math.abs(distance) / speed;
+  
+      function step(currentTime) {
+        const elapsedTime = currentTime - startTime;
+        const progress = Math.min(elapsedTime / duration, 1);
+        window.scrollTo(0, startPosition + distance * progress);
+  
+        if (progress < 1) {
+          requestAnimationFrame(step);
+        }
+      }
+  
+      requestAnimationFrame(step);
+    }
+  }
+  
+  // Add an event listener to handle clicks on anchor links
+  const navLinks = document.querySelectorAll('.nav-item a');
+  navLinks.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      const target = link.getAttribute('href');
+      const speed = 3; // Adjust the speed as needed (pixels per millisecond)
+      scrollToTarget(target, speed);
+    });
+  });
+  
